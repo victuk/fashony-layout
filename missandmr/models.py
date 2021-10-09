55127f6ff777cc34e623d94c3473627d263a20c2
victuk <ukokjnr@gmail.com>
2021-10-09 15:55:22 +0100

    Added form for contestants to fill to participate in the competition

--- missandmr/models.py
+++ missandmr/models.py
@@ -18,6 +18,11 @@ class contestant(models.Model):
     ], max_length = 10, default="Male")
     description = models.TextField(max_length=455, default='')
     proofOfPayment = models.ImageField(upload_to='images/proof-of-payment', default='')
+    status = models.TextField(
+        choices = [
+        ('Not Paid', 'Not Paid'),
+        ('Paid', 'Paid')
+    ],max_length=20, default='Not Paid')
     def __str__(self):
         return self.name
 
