e29f79021d0a9f1861611901d18cb35d3cf8c732
victuk <ukokjnr@gmail.com>
2021-10-09 16:12:58 +0100

    Added form for contestants to fill to participate in the competition

--- winnersDjaApp/settings.py
+++ winnersDjaApp/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Application definition
 
